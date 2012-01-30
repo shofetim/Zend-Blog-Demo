8fe7a68f81022199a497ee61542a43e038f7438f
Jordan Schatz <jordan@noionlabs.com>
2012-01-30 12:43:23 -0700

    WIP posts controller, model, and list view

--- application/controllers/PostsController.php
+++ application/controllers/PostsController.php
@@ -1,12 +1,27 @@
 <?php
 
 class PostsController extends Zend_Controller_Action {
-  public function init()
-  {
-    /* Initialize action controller here */
+  public function init() {
+    $this->Post = new Application_Model_Post;
   }
 
   public function indexAction() {
-    echo 'HELLO WORLD';
+    $this->view->Posts = $this->Post->fetchAll();
   }
+
+  public function view($id) {
+    $this->view->Post = $this->Post->fetchOne($id);
+  }
+
+  public function add() {
+  }
+
+  public function edit($id) {
+    $this->view->Post = $this->Post->fetchOne($id);
+  }
+
+  public function delete($id) {
+    $this->Post->delete($id);
+  }
+
 }
