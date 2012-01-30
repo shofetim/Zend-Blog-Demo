7a24fde93cbcbee8072717d14914bb6e1dc29e66
Jordan Schatz <jordan@noionlabs.com>
2012-01-30 12:18:38 -0700

    Added route for posts index to be front page

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -4,5 +4,12 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap {
   protected function _initAll() {
     $this->bootstrap('db');  
     Zend_Registry::set('db', $this->getResource('db'));
+    $route = new Zend_Controller_Router_Route('/',
+                                              array(
+                                                    'controller' => 'posts',
+                                                    'action' => 'index',
+                                                    )
+                                              );
+    Zend_Controller_Front::getInstance()->getRouter()->addRoute('/', $route);
   }
 }
