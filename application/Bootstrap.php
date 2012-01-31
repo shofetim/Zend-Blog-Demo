3f8072f4f97e3ea0522b341fda2c2c326833a981
Jordan Schatz <jordan@noionlabs.com>
2012-01-30 17:11:35 -0700

    Bootstrap comments

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -2,8 +2,11 @@
 
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap {
   protected function _initAll() {
+    //DB Setup
     $this->bootstrap('db');  
     Zend_Registry::set('db', $this->getResource('db'));
+    
+    //Front page should be a list of posts
     $route = new Zend_Controller_Router_Route('/',
                                               array(
                                                     'controller' => 'posts',
@@ -12,4 +15,5 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap {
                                               );
     Zend_Controller_Front::getInstance()->getRouter()->addRoute('/', $route);
   }
+
 }
